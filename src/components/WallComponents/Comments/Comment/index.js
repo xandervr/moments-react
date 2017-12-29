35b584f3a9b7be2e0e5bf0d85cbf426988d3bcca
xandervr <xandervanraemdonck@gmail.com>
2017-12-29 19:35:46 +0100

    Comments removable

--- src/components/WallComponents/Comments/Comment/index.js
+++ src/components/WallComponents/Comments/Comment/index.js
@@ -3,8 +3,23 @@ import TimeAgo from 'react-timeago';
 import {Link} from 'react-router-dom';
 import './index.css';
 
-const Comment = ({comment, ...props}) => {
-    return (
+const Comment = ({comment, deleteComment, currentUser, ...props}) => {
+    return currentUser._id === comment.user._id ? (
+        <li className="comment-container">
+            <div>
+                <span className="username comment-username">
+                    <Link to={`/u/${comment.user._id}`}>{comment.user.surname + ' ' + comment.user.name}</Link>
+                </span>
+                <span className="comment-text">{comment.text}</span>
+            </div>
+            <span className="comment-time">
+                <TimeAgo date={comment.created_on} />
+            </span>
+            <a className="delete-comment" onClick={() => deleteComment(`${comment._id}`)}>
+                x
+            </a>
+        </li>
+    ) : (
         <li className="comment-container">
             <div>
                 <span className="username comment-username">

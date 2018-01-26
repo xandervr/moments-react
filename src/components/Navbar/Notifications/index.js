b8e72460c8e55663cbd147c52f99d341d96bbf1b
jariverswyvel <jariverswyvel@hotmail.com>
2018-01-26 15:59:00 +0100

    notifications init

--- src/components/Navbar/Notifications/index.js
+++ src/components/Navbar/Notifications/index.js
@@ -1,20 +1,29 @@
-import React, {Component} from 'react';
+import React, {Fragment} from 'react';
+import {Link} from 'react-router-dom';
 import './index.css';
 
-class Notifications extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    render() {
-        return (
-            <div className="notifications-holder">
-                <div>notification</div>
-                <div>notification</div>
-            </div>
-        );
-    }
-}
+const Notifications = ({notifications}) => {
+    return (
+        <div className="notifications-holder">
+            {notifications.map((notification, index) => (
+                <Fragment key={index}>
+                    <div className="notification">
+                        <Link className="notifier" to={`/u/${notification.user_notifier._id}`}>
+                            <img src={notification.user_notifier.picture.image} alt="" />
+                        </Link>
+                        <p>{notification.text}</p>
+                        {notification.type === `boost` ||
+                            (notification.type === `comment` && (
+                                <Link to={`/e/${notification.experience._id}`}>
+                                    <img src={notification.experience.media.image} alt="" />
+                                </Link>
+                            ))}
+                    </div>
+                    {index !== notifications.length - 1 && <div className="notifications-divider" />}
+                </Fragment>
+            ))}
+        </div>
+    );
+};
 
 export default Notifications;
